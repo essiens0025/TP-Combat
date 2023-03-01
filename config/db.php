a7278c257e86a69a6e05e316130f593935b2d836
Serhat <119849836+Serhatman@users.noreply.github.com>
2023-03-01 10:45:36 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,9 +4,9 @@
 try
 {
 	$db = new PDO(
-        'mysql:host=localhost;dbname=tpcombat;charset=utf8', 
-        'root', 
-        '',
+        'mysql:host=57.128.65.58;dbname=ahmed_semih_serhat_tpC;charset=utf8', 
+        'serhat', 
+        'Serhat10',
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
     );
 }
